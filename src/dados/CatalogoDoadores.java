47e8c07dea924d664d30720102707b9bc3f12644
lucashenz <lucasberselihenz@yahoo.com.br>
2025-10-08 20:30:51 -0300

    [FT] leitura de arquivos e catalogo de doadores

--- src/dados/CatalogoDoadores.java
+++ src/dados/CatalogoDoadores.java
@@ -1,4 +1,42 @@
 package dados;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CatalogoDoadores {
+    private ClientInfoStatus<Doador> doadores;
+
+    public CatalogoDoadores{
+        doadores = new ArrayList<>();
+    }
+
+    public void lerArquivo() {
+        Path path = Paths.get("recursos/doadores.csv");
+        try(BufferedReader bufferedReader = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+
+            while ((linha = bufferedReader.readLine()) != null)) {
+                //separador
+                Scanner tec = new Scanner(linha).useDelimiter(";"); //para em ;
+                String nome;
+                String email;
+                nome = tec.next();
+                email = tec.next();
+
+                System.out.println(nome + ": " + email + "; ");
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro");
+        }
+    }
+
+
 }
