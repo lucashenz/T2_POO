04f42569ac7553e516ebd9e72a2a48be3940cf02
lucashenz <lucasberselihenz@yahoo.com.br>
2025-10-16 18:21:29 -0300

    [FT] segundo metodo

--- src/dados/CatalogoDoacoes.java
+++ src/dados/CatalogoDoacoes.java
@@ -1,8 +1,23 @@
 package dados;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
 public class CatalogoDoacoes {
     private Collection<Doacao> doacao;
+
+    public CatalogoDoacoes(ArrayList doacao){
+        doacao = new ArrayList<>();
+    }
+
+
+
 
 }
